db9425de8204af4c910028254d93ddb2cbf54ec6
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-08 21:36:44 -0300

    refactor: move server error to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,8 +1,15 @@
 import { MissingParamError } from '../errors/missing-param-erros'
+import { ServerError } from '../errors/server-erros'
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: MissingParamError): HttpResponse => ({
   statusCode: 400,
   body: error
+
+})
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
 
 })

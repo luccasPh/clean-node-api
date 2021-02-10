cfb120d7a252d2dcbbf5f00d05d54d8c3d622e82
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-09 22:33:16 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,17 @@
+import { MongoClient } from 'mongodb'
+import unknown from '../../../../../jest.integration.config'
+
+export const MongoHelper = {
+  client: unknown as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  }
+}

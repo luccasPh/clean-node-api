adf3ffdd044b69b3db25371311734e97259a1916
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-10 21:21:47 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    if (process.env.MONGO_URL) {
+      await MongoHelper.connect(process.env.MONGO_URL)
+    }
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('account')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account success', async () => {
     await request(app)
       .post('/api/signup')

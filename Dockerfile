b627d9aebe1c1d4f77f8339a3495b90af28e3ddf
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-15 13:51:05 -0300

    chore: add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,3 @@ WORKDIR /usr/src/clean-node-api
 COPY ./package.json .
 
 RUN yarn install --prod
-
-COPY ./dist ./dist
-
-EXPOSE 5000
-
-CMD yarn start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongodb:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecretKey: process.env.JWT_SECRET_KEY || 'ur$z192sb9jea4h8!dckg8o)ex*s8*r2$x^2cjs&k6&rt)m^l#'
 }

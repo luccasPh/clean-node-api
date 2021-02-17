9a9f2e141a227e25558ce36b4e0e7fa717134c46
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-17 16:57:01 -0300

    feat: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
+import { adapterMiddleware } from '../adapters/express-middleware-adapter'
 import { adapterRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middlewares-factory'
 
 export default (router: Router): void => {
-  router.post('/surveys', adapterRoute(makeAddSurveyController()))
+  const adminAuthMiddleware = adapterMiddleware(makeAuthMiddleware('admin'))
+  router.post('/surveys', adminAuthMiddleware, adapterRoute(makeAddSurveyController()))
 }

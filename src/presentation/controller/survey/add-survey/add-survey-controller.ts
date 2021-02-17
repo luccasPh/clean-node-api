fc6f69b78d1393415a5778b449dad43c65fc00cc
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-16 21:41:08 -0300

    feat: ensure AddSurveyController returns 500 if AddSurveyUseCase throws

--- src/presentation/controller/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controller/survey/add-survey/add-survey-controller.ts
@@ -1,5 +1,5 @@
 import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../../helpers/http/http-helper'
 
 export class AddSurveyController implements Controller {
   private readonly validation: Validation
@@ -11,18 +11,22 @@ export class AddSurveyController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest)
+      if (error) {
+        return badRequest(error)
+      }
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({
+        question,
+        answers
+      })
+      return await new Promise(resolve => resolve({
+        statusCode: 400,
+        body: 'any'
+      }))
+    } catch (error) {
+      return serverError(error)
     }
-    const { question, answers } = httpRequest.body
-    await this.addSurvey.add({
-      question,
-      answers
-    })
-    return await new Promise(resolve => resolve({
-      statusCode: 400,
-      body: 'any'
-    }))
   }
 }

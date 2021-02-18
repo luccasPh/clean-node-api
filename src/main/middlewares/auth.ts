bd6cdf492e9cdaff7513bc4c2e48fc09941ef3e6
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-18 16:00:34 -0300

    refactor: move auth middlewares to sperate files

---
+++ src/main/middlewares/auth.ts
@@ -0,0 +1,4 @@
+import { adapterMiddleware } from '../adapters/express-middleware-adapter'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middlewares-factory'
+
+export const auth = adapterMiddleware(makeAuthMiddleware(''))
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,15 +1,12 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import { adapterMiddleware } from '../adapters/express-middleware-adapter'
 import { adapterRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middlewares-factory'
+import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const adminAuthMiddleware = adapterMiddleware(makeAuthMiddleware('admin'))
-  const authMiddleware = adapterMiddleware(makeAuthMiddleware('admin'))
-
-  router.post('/surveys', adminAuthMiddleware, adapterRoute(makeAddSurveyController()))
-  router.get('/surveys', authMiddleware, adapterRoute(makeLoadSurveysController()))
+  router.post('/surveys', adminAuth, adapterRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adapterRoute(makeLoadSurveysController()))
 }

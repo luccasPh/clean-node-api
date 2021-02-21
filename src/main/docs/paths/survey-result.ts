28ab7a62c7ed69e35b48afb7efac473f8ded49bb
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-21 14:46:28 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result.ts
+++ src/main/docs/paths/survey-result.ts
@@ -44,5 +44,41 @@ export const surveyResultPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }
+    ],
+    tags: ['Survey'],
+    summary: 'Endpoint para consultar o resultado de uma enquete',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }

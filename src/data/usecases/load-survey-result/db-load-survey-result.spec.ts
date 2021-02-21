3cc2425e40200be3d5208df9bf040998d3bb0f24
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-21 10:45:17 -0300

    test: ensure DbLoadSurveyResult calls LoadSurveyREsultREpository with correct values

---
+++ src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,91 @@
+import MockDate from 'mockdate'
+
+import { DbLoadSurveyResult } from './db-load-survey-result'
+import { LoadSurveyResultRepository, SurveyResultModel } from './db-load-survey-result-protocols'
+
+const makeLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+      return await Promise.resolve({
+        surveyId: 'any_surveyId',
+        question: 'an_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer',
+          count: 10,
+          percent: 8
+        }, {
+          image: 'any_image',
+          answer: 'other_answer',
+          count: 8,
+          percent: 3
+        }],
+        date: new Date()
+      })
+    }
+  }
+  return new LoadSurveyResultRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = makeLoadSurveyResultRepository()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+  return {
+    sut,
+    loadSurveyResultRepositoryStub
+  }
+}
+
+describe('DbLoadSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call LoadSurveyResultRepository with correct values', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+    await sut.load('any_surveyId')
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_surveyId')
+  })
+
+  // test('Should throw if LoadSurveyResultRepository throws', async () => {
+  //   const { sut, loadSurveyResultRepositoryStub } = makeSut()
+  //   jest.spyOn(loadSurveyResultRepositoryStub, 'save').mockImplementationOnce(() => {
+  //     throw new Error()
+  //   })
+  //   const surveyResultData = makeSurveyResultData()
+  //   const promise = sut.save(surveyResultData)
+  //   await expect(promise).rejects.toThrow()
+  // })
+
+  // test('Should return SurveyResult on success', async () => {
+  //   const { sut } = makeSut()
+  //   const surveyResultData = makeSurveyResultData()
+  //   const surveyResult = await sut.save(surveyResultData)
+  //   expect(surveyResult).toEqual({
+  //     surveyId: 'any_surveyId',
+  //     question: 'an_question',
+  //     answers: [{
+  //       image: 'any_image',
+  //       answer: 'any_answer',
+  //       count: 10,
+  //       percent: 8
+  //     }, {
+  //       image: 'any_image',
+  //       answer: 'other_answer',
+  //       count: 8,
+  //       percent: 3
+  //     }],
+  //     date: new Date()
+  //   })
+  // })
+})

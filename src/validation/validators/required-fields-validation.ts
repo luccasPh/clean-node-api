99f7e06fc13599bfe8a9d08a9f7c8ac5a83617bc
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-22 15:41:07 -0300

    feat: ensure RequiredFields Validation return a MissingParamError if schema validation fails

--- src/validation/validators/required-fields-validation.ts
+++ src/validation/validators/required-fields-validation.ts
@@ -1,6 +1,7 @@
 import { AnySchema } from 'yup'
 
 import { Validation } from '@/presentation/protocols'
+import { InvalidParamError, MissingParamError } from '@/presentation/errors'
 
 export class RequiredFieldsValidation implements Validation {
   private readonly schema: AnySchema
@@ -9,10 +10,16 @@ export class RequiredFieldsValidation implements Validation {
     this.schema = schema
   }
 
-  validate (input: any): Error | null {
-    this.schema.isValid(input).catch(error => {
-      console.log(error)
-    })
-    return null
+  async validate (input: any): Promise<Error | null> {
+    try {
+      await this.schema.validate(input)
+      return null
+    } catch (err) {
+      if (err.type === 'required') {
+        return new MissingParamError(err.errors[0])
+      } else {
+        return new InvalidParamError(err.errors[0])
+      }
+    }
   }
 }
